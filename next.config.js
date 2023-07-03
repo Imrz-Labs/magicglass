9bf75ca630157079d63d06c5ea3f7becdb137f75
benemmaofficial <emmaben676@gmail.com>
2023-07-03 16:24:14 +0100

    next config update

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    fontLoaders: [
-      {
-        loader: "@next/font/google",
-        options: {
-          subsets: ["latin"],
-          style: ["normal", "bold"],
-          weight: ["400", "700"],
-        },
-      },
-    ],
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
   },
-};
-
-module.exports = nextConfig;
+}
+ 
+module.exports = nextConfig
